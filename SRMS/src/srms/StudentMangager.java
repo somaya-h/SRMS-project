0753c47fe11cc66c54979aec4cd4ee02a1cb12df
jory <jkxloe3@gmail.com>
2025-12-09 05:27:36 +0300

    add

--- SRMS/src/srms/StudentMangager.java
+++ SRMS/src/srms/StudentMangager.java
@@ -1,6 +1,11 @@
-
 package srms;
+import java.util.ArrayList;
 
 public class StudentMangager {
     
+    private ArrayList<Student> students;
+    
+    public StudentMangager(){
+        students = new ArrayList<>();
+        
 }
